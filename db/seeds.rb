f7c418a51da5c16cf82a6fe3275eec8f14a999da
Diogoloiola <diogomouraloiola@gmail.com>
2022-01-19 17:04:28 -0300

    created: script for populate database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Card.destroy_all
+IssuingBank.destroy_all
+IssuingBankPart.destroy_all
+
+10.times do
+  user = User.create!(name: Faker::Name.unique.name, email: Faker::Internet.email,
+                      cpf: Faker::Number.number(digits: 11).to_s)
+
+  credit_cart = Card.create!(holder_name: user.name, number: Faker::Number.number(digits: 5),
+                             expiration: '20/10/2022', cvv: Faker::Number.number(digits: 5).to_s, user_id: user.id)
+
+  issuing_bank = IssuingBank.create!(name: Faker::Name.unique.name)
+
+  IssuingBankPart.create!(card_id: credit_cart.id, issuing_bank_id: issuing_bank.id, status: true, balance: 2000.0)
+end
