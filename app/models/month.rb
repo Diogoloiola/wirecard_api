440439d0548c19e54066585508014d4cbf5c9df7
Diogoloiola <diogomouraloiola@gmail.com>
2022-01-20 22:36:31 -0300

    creating: enum according to each month's code

--- app/models/month.rb
+++ app/models/month.rb
@@ -1,3 +1,18 @@
 class Month < ApplicationRecord
   belongs_to :year
+
+  enum month_code: {
+    janeiro: 0,
+    feveriro: 1,
+    março: 2,
+    abril: 3,
+    maio: 4,
+    junho: 5,
+    julho: 6,
+    agosto: 7,
+    setembro: 8,
+    outubro: 9,
+    novembro: 10,
+    dezembro: 11
+  }
 end
