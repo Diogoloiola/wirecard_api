c00c661fd7f1ba0931ebc167387ae3a1ba7da578
Diogoloiola <diogomouraloiola@gmail.com>
2022-01-20 22:37:34 -0300

    created: function that creates the due date automatically

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,8 +1,14 @@
 class Payment < ApplicationRecord
   belongs_to :user
 
+  before_create :set_due
+
   enum billing_type: {
     boleto: 0,
     credit_card: 1
   }
+
+  def set_due
+    self.due_date = Time.zone.now + 1.month
+  end
 end
