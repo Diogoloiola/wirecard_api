bcb24fc2494f1ab39a57dc9198b502f7c3412730
Diogoloiola <diogomouraloiola@gmail.com>
2022-01-25 17:20:08 -0300

    created: function that creates a year if it does not exist

--- app/models/year.rb
+++ app/models/year.rb
@@ -1,4 +1,11 @@
 class Year < ApplicationRecord
   has_many :months, dependent: :destroy
   validates :year, presence: true
+
+  def self.create_year_and_months
+    year = Year.create!(year: Time.zone.now.strftime('%Y'))
+    12.times do |month_code|
+      Month.create!(month_code: month_code, year_id: year.id)
+    end
+  end
 end
